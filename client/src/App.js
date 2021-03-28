be457f3c7bf22283d41401686ced475151454066
Joni <58885165+JonHajdini@users.noreply.github.com>
2021-03-28 03:21:43 +0200

    MERN

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,25 @@
 
-import React from 'react';
+import React , { useEffect } from 'react';
 import {Container , AppBar , Typography ,Grow, Grid} from '@material-ui/core';
+import {useDispatch} from 'react-redux';
 
+import { getPosts} from './components/Posts/Posts';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import fotek  from './images/fotek.png';
-
+import useStyles from './styles';
 const App =() => {
+    const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+     dispatch(getPosts());
+    }, [dispatch]);
     return (
        <Container maxidth="lg">
-            <AppBar position="static" color="inherit">
-                <Typography variant="h2" align="center">Fotek</Typography>
-                 <img src={fotek} alt="fotek" heigth="60" />
+            <AppBar className = {classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">Fotek</Typography>
+                 <img className ={classes.image} src={fotek} alt="fotek" height="60" />
             </AppBar>
             <Grow in>
                 <Container>
